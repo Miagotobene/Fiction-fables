3288f920a3fd01f148fc181c9e877859b5e9c240
Miagotobene <miagotobene@gmail.com>
2024-08-10 00:35:24 -0400

    feat: added search functionality for blog and category models

--- blog/admin.py
+++ blog/admin.py
@@ -4,13 +4,24 @@ from .models import Comment
 from .models import Author
 from .models import Category
 
+# Add search functionality for blogs
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'intro', 'content']
+    list_display = ['title', 'slug', 'category', 'created_on']
+    list_filter = ['category', 'created_on']
+
+
+#  Add search functionality for categories
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    list_display = ['title']
 
 
 # Register your models here.
-admin.site.register(Blog)
+admin.site.register(Blog, PostAdmin)
 admin.site.register(Comment)
 admin.site.register(Author)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 
 
 
